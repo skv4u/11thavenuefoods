b2cdc73814e0e4584ec3a01ca24a13ca12d137a8
gopi <gopim@nalashaa.com>
2020-06-20 11:18:20 +0530

    Rice page added

---
+++ view/rice.php
@@ -0,0 +1,32 @@
+<!--inner banner---->
+<section id="innerbanner" style="overflow: hidden;">
+  <nav data-depth="2" class="breadcrumb">
+    <div class="container">
+      <h2>About Us</h2>
+      <ol>
+        <li> <a href="#"> <span>Home</span> </a> </li>
+        <li> <a href="#"> <span>About Us</span> </a> </li>
+      </ol>
+    </div>
+    <div class="category-cover hidden-sm-down"> <img class="img-fluid" alt="" src="img/fruit.png" style="display: inline;"> </div>
+  </nav>
+</section>
+<!--innner banned ends---> 
+<!--contact form---->
+<section class="contact padtb-100">
+  <div class="container">
+    <div class="row pl-0">
+      <div class="col-md-12">
+		   <img src="img/fruits.png" class="img-fluid float-right">
+        <h3 class=" mb-4">RICE</h3>
+       <p>Rice is a staple in any kitchen, and with so many different types, it lends itself to a limitless number of recipes. Before you add the same type of rice to every recipe you prepare, however, think twice. Substituting one type of rice for another can really alter the result of a recipe. Each type of rice has its own taste, texture, and unique properties that work well with different cooking applications. So how do you know which one is the best type of rice to use? This guide examines factors that differentiate types of rice, from nutty basmati to fragrant jasmine and more!</p>
+<p>
+
+		
+		
+      </div>
+      
+    </div>
+  </div>
+</section>
+<!--contact form ends--->
