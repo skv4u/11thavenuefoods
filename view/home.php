3aef7391b50b34fe7eb0569e495f2119494fbc23
gopi <gopim@nalashaa.com>
2020-06-20 17:12:50 +0530

    home

--- view/home.php
+++ view/home.php
@@ -8,21 +8,24 @@
   <div class="carousel-inner">
     <div class="carousel-item active"> <img src="img/slider-1.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <h1>Live With Organic Food</h1>
+        <h1>Live Healthy</h1>
+        <p>Your journey to ta healthier life starts here.</p>
       </div>
     </div>
-    <div class="carousel-item"> <img src="img/slider-1.jpg" class="d-block w-100" alt="...">
+
+    <div class="carousel-item"> <img src="img/slider-2.png" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <h1>Mango Pulp</h1>
+        <p style="color:#4e640c !important;">This is the sweet, golden pulp derived from Indian Alphonso mangoes</p>
       </div>
-    </div>
+    </div><!--
     <div class="carousel-item"> <img src="img/slider-1.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
       </div>
+-->
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>
